b75d72f0e53d6ef4297d77907b60ea9dd7e1c86c
Maicon Cerutti <maicon.cerutti@univille.br>
2017-10-16 23:17:29 -0200

    Primeira Swing - 4 Bimestre

---
+++ Exercícios POO Faculdade/Projeto Swing/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MainGUI janela = new MainGUI();
+		
+		janela.setVisible(true);
+		
+		
+
+	}
+
+}
