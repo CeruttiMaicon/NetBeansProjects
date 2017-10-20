4592d2ff5f779b52b05f7ce57f1ce06e45822e63
Maicon Cerutti <maicon.cerutti@univille.br>
2017-10-20 17:00:10 -0200

    Curso POO Java – 15a+b – Projeto Final em Java (Parte 2)

--- CURSO_EM_VÍDEO_JavaPOO/ProjetoYoutube/src/projetoyoutube/ProjetoYoutube.java
+++ CURSO_EM_VÍDEO_JavaPOO/ProjetoYoutube/src/projetoyoutube/ProjetoYoutube.java
@@ -16,8 +16,26 @@ public class ProjetoYoutube {
         g[0] = new Gafanhoto("Maicon", 19, "M", "cerutti.maicon");
         g[1] = new Gafanhoto("Creuza", 12, "M", "creuzita");
         
+        /*
+        System.out.println("VÍDEOS \n---------------------");
         System.out.println(v[0].toString());
+        System.out.println(v[1].toString());
+        System.out.println(v[2].toString());
+        System.out.println("\nGAFANHOTOS \n-----------------------");
         System.out.println(g[0].toString());
+        System.out.println(g[1].toString()); */
+        
+        //Maicon assistiu o video de HTML
+        Visualizacao vis[] = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[2]);
+        vis[0].avaliar(87.0f);
+        System.out.println(vis[0].toString());
+        
+        //Maicon assistiu o video de PHP
+        vis[1] = new Visualizacao(g[0], v[1]);
+        vis[1].avaliar();
+        System.out.println(vis[1].toString());
+        
         
     }
     
